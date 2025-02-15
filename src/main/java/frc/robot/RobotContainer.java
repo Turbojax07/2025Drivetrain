c3ff34a2fac346adc6fcca6805d12e8d824b05be
turbojax07 <turbojax07@gmail.com>
2025-02-14 21:09:46 -0500

    Adjusting imports and motor configs

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,20 +3,17 @@ package frc.robot;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.Subsystems.Drivetrain.Drivetrain;
-import frc.robot.Subsystems.Drivetrain.ModuleIOSim;
-import frc.robot.Subsystems.Drivetrain.ModuleIOTalonFX;
-import frc.robot.Subsystems.Drivetrain.Commands.SwerveDrive;
-import frc.robot.Subsystems.Gyro.Gyro;
-import frc.robot.Subsystems.Gyro.GyroIOBNO085;
+import frc.robot.Subsystems.Drivetrain.*;
+import frc.robot.Subsystems.Drivetrain.Commands.*;
+import frc.robot.Subsystems.Gyro.*;
 
 public class RobotContainer {
     private CommandXboxController controller = new CommandXboxController(0);
 
     public RobotContainer() {
         if (RobotBase.isReal()) {
-            new Gyro(new GyroIOBNO085());
-            new Drivetrain(new ModuleIOTalonFX(0), new ModuleIOTalonFX(1), new ModuleIOTalonFX(2), new ModuleIOTalonFX(3));
+            new Gyro(new GyroIOPigeon2());
+            new Drivetrain(new ModuleIOSparkMax(0), new ModuleIOSparkMax(1), new ModuleIOSparkMax(2), new ModuleIOSparkMax(3));
         } else {
             new Drivetrain(new ModuleIOSim(0), new ModuleIOSim(1), new ModuleIOSim(2), new ModuleIOSim(3));
         }
